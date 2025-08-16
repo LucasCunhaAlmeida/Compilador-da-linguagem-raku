0ba1248b1818ab5241113b25bc0f3d7436359f2c
heveeedesouza <156838385+heveeedesouza@users.noreply.github.com>
2025-08-16 20:49:44 -0300

    Update sintaxeabstrataa.py

--- docs/src/sintaxeabstrata.py
+++ docs/src/sintaxeabstrata.py
@@ -18,3 +18,56 @@ def __init__(self, comando):
 self.comando = comando
 def accept(self):
   pass
+
+# Estruturas de Repetição
+class loopFor(Loop):
+    def __init__(self, expr, id, comando):
+        self.expr = expr         
+        self.id= id           
+        self.comando = comando    
+    
+    def accept(self):
+        pass
+
+
+class loopTimes(Loop):
+    def __init__(self, integer, id, comando):
+        self.integer = integer
+        self.id = id
+        self.comando = comando
+    
+    def accept(self):
+        pass
+
+
+class loopWhile(Loop):
+    def __init__(self, id, limite, comando):
+        self.id = id         
+        self.limite = limite    
+        self.comando = comando  
+    
+    def accept(self):
+        pass
+
+class loopRepeticao(Loop):
+    def __init__(self, instrucao1, instrucao2, instrucao3, comando):
+        self.instrucao1 = instrucao1   
+        self.instrucao2 = instrucao2   
+        self.instrucao3 = instrucao3   
+        self.comando = comando
+    
+    def accept(self):
+        pass
+
+class loopSemCondicao(Loop):
+    def __init__(self, comando):
+        self.comando = comando
+    
+    def accept(self):
+        pass
+
+
+
+
+
+
