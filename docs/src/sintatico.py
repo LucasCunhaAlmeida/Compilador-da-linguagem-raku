3782f0476039f9560fb6182936a214125d768ff1
Miyamura <felipesilsou1101@gmail.com>
2025-08-17 22:18:34 -0300

    Criação da funções abstratas no arquivo sintaceabstrata.py

--- docs/src/sintatico.py
+++ docs/src/sintatico.py
@@ -1,5 +1,6 @@
 import ply.yacc as yacc
 import lexico as lex
+import sintaxeabstrata as sa
 tokens = lex.tokens
 
 def p_exp(p):
@@ -15,11 +16,11 @@ def p_exp_2(p):
 
 def p_or(p):
    '''or : exp_2 OR_S exp_3'''
-   pass
+   p[0] = sa.ExpressaoOR(p[1], p[3])
 
 def p_xor(p):
    '''xor : exp_2 XOR_S exp_3'''
-   pass
+   p[0] = sa.ExpressaoXOR(p[1], p[3])
 
 def p_exp_3(p):
    '''exp_3 : and
@@ -28,7 +29,7 @@ def p_exp_3(p):
 
 def p_and(p):
    '''and : exp_3 AND_S exp_4'''
-   pass
+   p[0] = sa.ExpressaoAND(p[1], p[3])
 
 def p_exp_4(p):
    '''exp_4 : igual_dp
@@ -43,31 +44,31 @@ def p_exp_4(p):
 
 def p_igual_dp(p):
    '''igual_dp : exp_4 IGUAL_DP exp_5'''
-   pass
+   p[0] = sa.ExpressaoIGUAL_DP(p[1], p[3])
 
 def p_dif(p):
     '''dif : exp_4 DIF exp_5'''
-    pass
+    p[0] = sa.ExpressaoDIF(p[1], p[3])
 
 def p_maior(p):
    '''maior : exp_4 MAIOR exp_5'''
-   pass
+   p[0] = sa.ExpressaoMAIOR(p[1], p[3])
 
 def p_menor(p):
    '''menor : exp_4 MENOR exp_5'''
-   pass
+   p[0] = sa.ExpressaoMENOR(p[1], p[3])
 
 def p_maior_igual(p):
    '''maior_igual : exp_4 MAIOR_IGL exp_5'''
-   pass
+   p[0] = sa.ExpressaoMAIOR_IGUAL(p[1], p[3])
 
 def p_menor_igual(p):
    '''menor_igual : exp_4 LESSEQUAL exp_5'''
-   pass
+   p[0] = sa.ExpressaoMENOR_IGUAL(p[1], p[3])
 
 def p_smartmatch(p):
    '''smartmatch : exp_4 SMARTMATCH exp_5'''
-   pass
+   p[0] = sa.ExpressaoSMARTMATCH(p[1], p[3])
 
 def p_exp_5(p):
     '''exp_5 : adicao
@@ -78,15 +79,15 @@ def p_exp_5(p):
 
 def p_adicao(p):
    '''adicao : exp_5 ADC exp_6'''
-   pass
+   p[0] = sa.ExpressaoADICAO(p[1], p[3])
 
 def p_subtracao(p):
    '''subtracao : exp_5 SUB exp_6'''
-   pass
+   p[0] = sa.ExpressaoSUBTRACAO(p[1], p[3])
 
 def p_conc(p):
    '''conc : exp_5 CONC exp_6'''
-   pass
+   p[0] = sa.ExpressaoCONCATENACAO(p[1], p[3])
 
 def p_exp_6(p):
     '''exp_6 : mult
@@ -101,31 +102,31 @@ def p_exp_6(p):
 
 def p_mult(p):
    '''mult : exp_6 MULT exp_7'''
-   pass
+   p[0] = sa.ExpressaoMULTIPLICACAO(p[1], p[3])
 
 def p_divide(p):
    '''divide : exp_6 DIVIDE exp_7'''
-   pass
+   p[0] = sa.ExpressaoDIVISAO(p[1], p[3])
 
 def p_div(p):
    '''div : exp_6 DIV exp_7'''
-   pass
+   p[0] = sa.ExpressaoDIVISAO_INTEIRA(p[1], p[3])
 
 def p_divi(p):
    '''divi : exp_6 DIVI exp_7'''
-   pass
+   p[0] = sa.ExpressaoDIVISIBILIDADE(p[1], p[3])
 
 def p_mod(p):
    '''mod : exp_6 MOD exp_7'''
-   pass
+   p[0] = sa.ExpressaoMOD(p[1], p[3])
 
 def p_lcm(p):
    '''lcm : exp_6 LCM exp_7'''
-   pass
+   p[0] = sa.ExpressaoLCM(p[1], p[3])
 
 def p_gcd(p):
    '''gcd : exp_6 GCD exp_7'''
-   pass
+   p[0] = sa.ExpressaoGCD(p[1], p[3])
 
 def p_exp_7(p):
     '''exp_7 : pow
@@ -134,7 +135,7 @@ def p_exp_7(p):
 
 def p_pow(p):
    '''pow : exp_7 POW exp_8'''
-   pass
+   p[0] = sa.ExpressaoPOW(p[1], p[3])
 
 def p_exp_8(p):
     '''exp_8 : not_op
@@ -144,11 +145,11 @@ def p_exp_8(p):
 
 def p_not_op(p):
    '''not_op : NOT exp_8 '''
-   pass
+   p[0] = sa.ExpressaoNOT_OPERADOR(p[2])
 
 def p_not_s(p):
    '''not_s : NEGAC exp_8'''
-   pass
+   p[0] = sa.Expressao_NOT_SIMBULO(p[2])
 
 def p_exp_9(p):
     '''exp_9 : unario
@@ -164,37 +165,32 @@ def p_unario(p):
 
 def p_prefixo_incremento(p):
    '''prefixo_incremento : ADC_DP ID'''
-   pass
+   p[0] = sa.Expressao_PREFIXO_INCREMENTO(p[2]) # ARQUI É 2 OU 1
 
 def p_posfixo_incremento(p):
     '''posfixo_incremento : ID ADC_DP'''
-    pass
+    p[0] = sa.Expressao_POSFIXO_INCREMENTO(p[1]) 
 
 def p_prefixo_decremento(p):
    '''prefixo_decremento : DECREMENTO ID'''
-   pass
+   p[0] = sa.Expressao_PREFIXO_DECREMENTO(p[2]) # AQUI É 2 OU 1
 
 def p_posfixo_decremento(p):
     '''posfixo_decremento : ID DECREMENTO'''
-    pass
+    p[0] = sa.Expressao_POSFIXO_DECREMENTO(p[1]) 
 
 def p_operando(p):
     '''operando : parenteses
-                | chaves
                 | tipo '''
     p[0] = p[1]
 
 def p_parentese(p):
    '''parenteses : LPAREN exp RPAREN'''
-   pass
-
-def p_chaves(p):
-   '''chaves : ABRE_CHAVE exp FECHA_CHAVE'''
-   pass
+   p[0] = sa.Expressao_PARENTESES(p[2]) 
 
 # Tipo 
 
-def p_tipo(p):
+def p_tipo(p):          # TEM QUE SER VALORES CONSTANTES
     '''tipo : inteiro
            | float
            | string
@@ -205,24 +201,24 @@ def p_tipo(p):
 def p_inteiro(p):
    '''inteiro : INTEGER
               | INT '''
-   p[0] = p[1]
+   p[0] = sa.Expressao_VALOR(p[1], 'int')
 
 def p_float(p):
    '''float : FLOAT'''
-   p[0] = p[1]
+   p[0] = sa.Expressao_VALOR(p[1], 'float')
 
 def p_string(p):
     '''string : STRING
               | STR'''
-    p[0] = p[1]
+    p[0] = sa.Expressao_VALOR(p[1], 'str')
 
 def p_boolean(p):
    '''boolean : BOOLEAN'''
-   p[0] = p[1]
+   p[0] = sa.Expressao_VALOR(p[1], 'boolean')
 
 def p_id(p):
    '''id : ID'''
-   p[0] = p[1]
+   p[0] = sa.Expressao_VALOR(p[1], 'ID')
 
 def p_error(p):
     print(f"Erro de Sintaxe: {p}")
@@ -250,32 +246,32 @@ def p_lista_valores(p):
 #id coloquei como a variavel
 def p_for(p):
     '''loop : FOR expr SETA ID ABRE_CHAVE comando FECHA_CHAVE''' 
-    p[0] = loopFor(p[2], p[4], p[6])
+    p[0] = sa.loopFor(p[2], p[4], p[6])
     
 def p_ponto_times (p):
     ''' loop : INTEGER PONTO TIMES SETA ID  ABRE_CHAVE comando FECHA_CHAVE '''
-    p[0] = loopTimes(p[1], p[5], p[7])
+    p[0] = sa.loopTimes(p[1], p[5], p[7])
 
 def p_while (p):
     ''' loop : WHILE ID LESSEQUAL INTEGER ABRE_CHAVE comando FECHA_CHAVE '''
-    p[0] = loopWhile(p[2], p[4], p[6])
+    p[0] = sa.loopWhile(p[2], p[4], p[6])
 
 def p_loop(p):
     '''loop : LOOP LPAREN instrucao PV instrucao PV instrucao RPAREN ABRE_CHAVE comando FECHA_CHAVE'''
-    p[0] = loopRepeticao(p[3], p[5], p[7], p[9])
+    p[0] = sa.loopRepeticao(p[3], p[5], p[7], p[9])
 
 def p_loop_sem_condicao(p):
     '''loop : LOOP ABRE_CHAVE comando FECHA_CHAVE'''
-    p[0] =  loopSemCondicao(p[3])
+    p[0] =  sa.loopSemCondicao(p[3])
 
 #Estrutura de uma função
 def p_funcao(p):
     '''funcao : FUNCTION ID LPAREN parametros RPAREN ABRE_CHAVE comando FECHA_CHAVE'''
-    p[0] = CompoundFuncao(p[4],p[7])
+    p[0] = sa.CompoundFuncao(p[4],p[7])
 
 def p_funcao_sem_parametros(p):
     '''funcao : FUNCTION ID LPAREN RPAREN ABRE_CHAVE comando FECHA_CHAVE'''
-    p[0] = CompoundFuncaoSemParametros(p[6])
+    p[0] = sa.CompoundFuncaoSemParametros(p[6])
 
 # Condicionais
 def p_condicional1(p):
