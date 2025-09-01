a4d64353fac222656fdeda1af98362ee2b7087f2
heveeedesouza <156838385+heveeedesouza@users.noreply.github.com>
2025-09-01 15:36:46 -0300

    Create visitor.py

---
+++ docs/src/visitor.py
@@ -0,0 +1,29 @@
+from AbstractVisitor import abstractVisitor
+
+class Visitor (abstractVisitor):
+
+#for 1..5 -> $x { ... }
+  #comentei pq estou vendo como irá ficar esse ainda
+    #def visitLoopFor(self, loop_for):
+       # print(f"for {loop_for.interger.interpolacao.interger} -> {loop_for.escalar}: ", end="")
+        #loop_for.comando.accept(self)
+    
+#5.times -> $teste { ... }
+    def visitLoopTimes(self, loop_times):
+        print(f"{loop_times.integer}.times -> {loop_times.id} ", end="")
+        loop_times.comando.accept(self)
+
+#while ($teste <= 5) { ... }
+    def visitLoopWhile(self, loop_while):
+        print(f"while ({loop_while.escalar} <= {loop_while.interger}) ", end="")
+        loop_while.comando.accept(self)   
+
+#loop (my $a = 0; $a < 4; $a++) { ... }
+    def visitLoopRepeticao(self, loop_repeticao):
+        print(f"loop ({loop_repeticao.instrucao1}; {loop_repeticao.instrucao2}; {loop_repeticao.instrucao3}) ", end="")
+        loop_repeticao.comando.accept(self)
+
+#loop { ... }
+    def visitLoopSemCondicao(self, loop_sem_condicao):
+        print("loop: ", end="")
+        loop_sem_condicao.comando.accept(self)
